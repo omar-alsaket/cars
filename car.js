af562f0c34b0cd3ea50ab9eee1f6b735f8a147d7
omarAlsaket <>
2021-06-19 18:14:03 +0000

    css

--- car.js
+++ car.js
@@ -1,18 +1,18 @@
 
 
-var name = prompt("what is your name")
+// var name = prompt("what is your name")
 
-alert(" welcome  " + name )
+// alert(" welcome  " + name )
 
-var favcar = prompt ("what is your favcar(mercedes or bmw or audi?")
+// var favcar = prompt ("what is your favcar(mercedes or bmw or audi?")
 
-if (favcar == "mercedes"){document.write("<p>"+"super cars is here" +"</p>") }
+// if (favcar == "mercedes"){document.write("<p>"+"super cars is here" +"</p>") }
 
-else if (favcar == "bmw")  {document.write("<p>"+"super cars is here"+"</p>") }
+// else if (favcar == "bmw")  {document.write("<p>"+"super cars is here"+"</p>") }
   
 
 
-else if (favcar =="audi")  {document.write("<p>"+"super cars is here"+"</p>") }
+// else if (favcar =="audi")  {document.write("<p>"+"super cars is here"+"</p>") }
   
 
-else {alert("sorry we dont have")}
+// else {alert("sorry we dont have")}
