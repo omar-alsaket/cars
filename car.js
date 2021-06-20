d23e72a3080f7992754ed5ff66c5b2252a28d908
omarAlsaket <>
2021-06-20 04:44:19 +0000

    while

--- car.js
+++ car.js
@@ -4,9 +4,13 @@ var name = prompt("what is your name")
 
 alert(" welcome  " + name )
 
+var favcar =prompt("what is your favcar here (bmw,audi,mercedes)?")
+
+while(favcar  != "mercedes"  && favcar != "bmw"  && favcar != "audi" ){ favcar = prompt ("sorry try again")}
+
 var favcar = prompt ("what is your favcar(mercedes or bmw or audi?")
 
-if (favcar == "mercedes"){document.write("<p>"+"super cars is here" +"</p>") }
+if (favcar == "mercedes"){  document.write("<p>"+"super cars is here" +"</p>") }
 
 else if (favcar == "bmw")  {document.write("<p>"+"super cars is here"+"</p>") }
   
@@ -16,3 +20,4 @@ else if (favcar =="audi")  {document.write("<p>"+"super cars is here"+"</p>") }
   
 
 else {alert("sorry we dont have")}
+
